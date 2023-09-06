482c078a55ce59b9248aa70e89a60370a0933fd7
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2023-09-07 01:40:13 +0800

    Create video_question.jsx

---
+++ src/components/video_question.jsx
@@ -0,0 +1,32 @@
+import "./video_question.scss";
+import TextareaAutosize from 'react-textarea-autosize';
+
+
+function VideoQuestion(){
+  
+
+    return(
+        <div className="video_question">
+            <span className="grip_dots"></span>
+            <form action="/" method="get" >
+                <div className="question_title">
+                    <div>                       
+                        <input type="text" name="title" placeholder="Type your question here."/>
+                        <span>drop</span>                  
+                    </div>
+                    <label>
+                        <span className="file_attachment_icon">O</span>
+                        <input type="file" name="attachment"/>
+                    </label>                   
+                </div>
+                <div className="video_input">
+                    <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </div>
+                <TextareaAutosize placeholder="Write notes that are only visible to you. (Optional)" maxRows="5"/>
+                <p className="required_note">Video questions are required.</p>
+            </form>
+        </div>  
+    )
+}
+
+export default VideoQuestion;
